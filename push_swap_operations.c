626373deddc04fc0fb4bc5114f4f721f6c73971b
Grzegorz Kryszczuk <gkryszcz@c3r7s3.42warsaw.pl>
2025-07-24 15:04:03 +0200

    added functions from 42

--- push_swap_operations.c
+++ push_swap_operations.c
@@ -58,3 +58,69 @@ void	reverse_rotate(t_list **head)
 	last->next = *head;
 	*head = last;
 }
+
+void decide(t_list **stacka, t_list **stackb)
+{
+	int size;
+	size = ft_lstsize(*stacka);
+	while(size > 3)
+	{
+		push(*stacka, *stackb);
+		size = ft_lstsize(*stacka);
+	}
+	if(size == 3)
+		sort_three(**stacka);
+	if(size < 3)
+	{
+		if((*stacka)->val > (*stacka)->next->val)
+			sa(stacka);
+	}
+}
+
+void minmax(t_list **stackb)
+{
+	int min;
+	int max;
+	if((*stackb)->val > (*stackb)->next->val)
+	{
+		min = (*stackb)->next->val;
+		max = (*stackb)->val;
+	}
+	else
+	{
+		max = (*stackb)->next->val;
+		min = (*stackb)->val;
+	}
+}
+
+t_list	*target_node(int value, t_list *stackb)
+{
+	int min;
+	int i;
+	int tmp;
+	int min_index;
+	t_list **current;
+
+	i = 0;
+	(*current)-> index = i;
+	min = stackb->val - value;
+	current = (*current)-> next;
+
+	while((*current)->next)
+	{	
+		(*current)-> index = i;
+		tmp = (*current)->val - value;
+		if (tmp < min)
+		{
+			min = tmp;
+			min_index = i;
+		}
+		i++;
+		current = (*current)-> next;
+	}
+	while(stackb->next)
+	{
+		if(stackb->index == min_index)
+			return stackb;
+	}
+}
